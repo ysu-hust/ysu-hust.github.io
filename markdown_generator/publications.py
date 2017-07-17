b9fc4020d7a4d9347776298b2f217de8dc3e5385
lavenresearch <lavenresearch@gmail.com>
2017-07-17 15:03:22 +0800

    fix

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -94,12 +94,12 @@ for row, item in publications.iterrows():
     
     if len(str(item.paper_url)) > 5:
         md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
+
+    md += "\nRecommended Citation: \n" + item.citation + "\n"
         
     if len(str(item.excerpt)) > 5:
         md += "\n__Abstract__:"
         md += "\n" + html_escape(item.excerpt) + "\n"
-        
-    # md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
